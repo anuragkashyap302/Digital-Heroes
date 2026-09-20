7070c698d06da0bc2b66427e1793d266fce649c3
anuragkashyap302 <kumaranuragg68@gmail.com>
2026-09-20 15:41:51 +0530

    implement Express server entry point with middleware and API routes

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ app.use(helmet({
 
 const allowedOrigins = [
   ENV.CLIENT_URL,
+  'https://digital-heroes-anurag.vercel.app',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://localhost:3000'
